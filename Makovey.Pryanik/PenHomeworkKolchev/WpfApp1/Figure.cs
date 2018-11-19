606842b26d73204a8162a31f308c4da9149f54ed
Egor Kolchev <kolchev-00@mail.ru>
2018-11-19 17:35:25 +1000

    add pen DZ

---
+++ Makovey.Pryanik/PenHomeworkKolchev/WpfApp1/Figure.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    class Figure
+    {
+        private List<Point> Coordinates;
+
+        public Figure()
+        {
+            InitList();
+        }
+
+        private void InitList()
+        {
+            Coordinates = new List<Point>();
+        }
+
+        public Figure(Point aPoint) : this()
+        {
+            Coordinates.Add(aPoint);
+        }
+
+        public void Draw(UIElementCollection p)
+        {
+            for (int i = 0; i < Coordinates.Count - 1; i++)
+            {
+                System.Windows.Shapes.Line line = new System.Windows.Shapes.Line()
+                {
+                    X1 = Coordinates[i].X,
+                    Y1 = Coordinates[i].Y,
+                    X2 = Coordinates[i + 1].X,
+                    Y2 = Coordinates[i + 1].Y,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                };
+                p.Add(line);
+            }
+        }
+
+        public void AddPoint(Point aPoint)
+        {
+            Coordinates.Add(aPoint);
+        }
+    }
+}
---
+++ Makovey.Pryanik/PenHomeworkKolchev/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Figure> figures;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            figures = new List<Figure>();
+        }
+        private void Invalidate()
+        {
+            Canvas.Children.Clear();
+            foreach (var figure in figures)
+                figure.Draw(Canvas.Children);
+        }
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                figures.Last().AddPoint(e.GetPosition((IInputElement)sender));
+                Invalidate();
+            }
+        }
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            figures.Add(new Figure(e.GetPosition((IInputElement)sender)));
+            Invalidate();
+        }
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
